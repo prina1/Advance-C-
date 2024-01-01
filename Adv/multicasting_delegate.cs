035f4a25984d60156db880ba3db9cbc4e802ed41
Prina Tamang <121554323+prina1@users.noreply.github.com>
2024-01-01 08:21:14 +0545

    Third Commit

--- Adv/multicasting_delegate.cs
+++ Adv/multicasting_delegate.cs
@@ -1,38 +1,38 @@
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
 
-delegate int NumberChanger(int n);
-class Testdelegate{
-    static int num = 10;
+// delegate int NumberChanger(int n);
+// class Testdelegate{
+//     static int num = 10;
 
-    public static int AddNum(int p)
-    {
-        num+= p;
-        return num;
-    }
-    public static int MulNum(int q)
-    {
-        num*= q;
-        return num;
-    }
-    public static int getNum()
-    {
-        return num;
-    }
-    static void Main(string[]args)
-    {
-        // creating delegate instances
-        NumberChanger nc;
-        NumberChanger nc1 = new NumberChanger(AddNum);
-        NumberChanger nc2 = new NumberChanger(MulNum);
-        nc = nc1;
-        nc += nc2;
+//     public static int AddNum(int p)
+//     {
+//         num+= p;
+//         return num;
+//     }
+//     public static int MulNum(int q)
+//     {
+//         num*= q;
+//         return num;
+//     }
+//     public static int getNum()
+//     {
+//         return num;
+//     }
+//     static void Main(string[]args)
+//     {
+//         // creating delegate instances
+//         NumberChanger nc;
+//         NumberChanger nc1 = new NumberChanger(AddNum);
+//         NumberChanger nc2 = new NumberChanger(MulNum);
+//         nc = nc1;
+//         nc += nc2;
 
-        //calling multicast 
-        nc(5);
-        Console.WriteLine("Value of Num: {0}", getNum());
-        Console.ReadKey();
+//         //calling multicast 
+//         nc(5);
+//         Console.WriteLine("Value of Num: {0}", getNum());
+//         Console.ReadKey();
 
-    }
+//     }
 
-}
+// }
