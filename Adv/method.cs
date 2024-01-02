0aba757cbbf543b15c886f1e3f8c38f1f428d85b
Prina Tamang <121554323+prina1@users.noreply.github.com>
2024-01-02 08:46:44 +0545

    Six: Commit

--- Adv/method.cs
+++ Adv/method.cs
@@ -1,29 +1,34 @@
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
 
-delegate int MathOperation(int x, int y);
+// delegate int MathOperation(int x, int y);
 
-class MyClass
-{
-    public int Add(int x, int y)
-    {
-        return x + y;
-    }
-}
+// class MyClass
+// {
+//     public int Add(int x, int y)
+//     {
+//         return x + y;
+//     }
+// }
 
-delegate void DisplayMessage(string message);
+// delegate void DisplayMessage(string message);
 
-class Program
-{
-    //methods compatible with the delegate 
-    static void ShowMessage(string message) => 
-    Console.WriteLine(message);
+// class Program
+// {
+//     //methods compatible with the delegate 
+//     static void ShowMessage(string message) => 
+//     Console.WriteLine(message);
 
-    static void Main(string[] args)
-    {
-        DisplayMessage displayDelegate = new DisplayMessage(ShowMessage);
-        displayDelegate("Hello, World!");
-    }
-}
+//     static void Main(string[] args)
+//     {
+//         DisplayMessage displayDelegate = new DisplayMessage(ShowMessage);
+//         displayDelegate("Hello, World!");
+
+//         MyClass myClassInstance = new MyClass();
+//         MathOperation mathDelegate = new MathOperation(myClassInstance.Add);
+//         int result = mathDelegate(5, 10);
+//         Console.WriteLine(result);
+//     }
+// }
 
 
