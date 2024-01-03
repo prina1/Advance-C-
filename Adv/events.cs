a63dbcb5c1fef2f856f7fac880d65c0b2dc99b4b
Prina Tamang <121554323+prina1@users.noreply.github.com>
2024-01-03 07:59:45 +0545

    add

--- Adv/events.cs
+++ Adv/events.cs
@@ -1,40 +1,42 @@
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
 
-namespace enventHandling1
-{
-    //declareing a delegate type for the event 
-    public delegate void DelEneventHandler();
-    class Program
-    {
-        public static event DelEneventHandler add;
+// namespace enventHandling1
+// {
+//     //declareing a delegate type for the event 
+//     public delegate void DelEneventHandler();
+//     class Program
+//     {
+//         public static event DelEneventHandler add;
 
-        static void Nepal()
-        {
-            Console.WriteLine("Nepal");
+//         static void Nepal()
+//         {
+//             Console.WriteLine("Nepal");
         
-        }
-        static void india()
-        {
-            Console.WriteLine("india");
+//         }
+//         static void india()
+//         {
+//             Console.WriteLine("india");
 
-        }
-        static void china()
-        {
-            Console.WriteLine("china");
+//         }
+//         static void china()
+//         {
+//             Console.WriteLine("china");
 
-        }
-        static void Main (string[] args)
-        {
-            add += new DelEneventHandler(Nepal);
-            add += new DelEneventHandler(india);
-            add += new DelEneventHandler(china);
-            add.Invoke();
-            Console.ReadKey();
-        }
+//         }
+//         static void Main (string[] args)
+//         {
+//             add += new DelEneventHandler(Nepal);
+//             add += new DelEneventHandler(india);
+//             add += new DelEneventHandler(china);
+//             add.Invoke();
+//             Console.ReadLine();
+//         }
 
 
 
-    }
+//     }
 
-}
+// }
+
+
